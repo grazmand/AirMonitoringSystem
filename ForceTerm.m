e618aaa096ba92d8b92b80559890ea7c6ed8f47c
graziano <graziano.manduzio@gmail.com>
2021-04-03 02:06:13 +0200

    ForceTerm class updated

---
+++ ForceTerm.m
@@ -0,0 +1,27 @@
+classdef ForceTerm < matlab.mixin.SetGet
+    properties (SetAccess = private, GetAccess = public)
+        %% basics
+        name string
+        sources Sources
+        time Time
+        %% derivative
+        force_term double
+    end
+    
+    methods
+        function forceTerm(obj, vals)
+            props = {'name','sources','time'};
+            obj.set(props, vals)
+            obj.setForceTerm()
+        end
+        
+        function initForceTerm(obj)
+            obj.force_term = zeros(obj.mesh.node_size_number,obj.time.time_steps);
+        end
+        
+        function setForceTerm(obj)
+            obj.initForceTerm()
+            obj.force_term(obj.sources.elementNodeIndexes,:) = - obj.sources.em_factor .* obj.sources.shapes;
+        end
+    end
+end
--- Sources.m
+++ Sources.m
@@ -1,24 +1,43 @@
 classdef Sources < matlab.mixin.SetGet
     properties (SetAccess = private, GetAccess = public)
+        name string
         roads Roads
+        mesh Mesh
+        fem FemModel
         coordinates string % NX2 array
         element_indexes {mustBeInteger}
         em_factor double = 0.1
+        shapes double
     end
     
     methods
-        function source(obj ,vals)
-            props = {'roads'};
+        function sources(obj ,vals)
+            props = {'name','roads','mesh','fem'};
             obj.set(props, vals)
+            obj.set_element_indexes()
+            obj.set_coordinates()
+            obj.set_shapes()
         end
         
         function set_element_indexes(obj)
-            ie=1;
-            for ib=1:size(obj.roads.blocks,2)
-                for ip=1:size(obj.roads.blocks{ib},2)
-                    element_indexes(ie)=obj.roads.
+            i_e=1;
+            for ib=1:size(obj.roads.element_blocks,2)
+                for ip=1:size(obj.roads.element_blocks{ib},2)
+                    for ie=1:size(obj.roads.element_blocks{ib}{ip},2)
+                        obj.element_indexes(i_e)=obj.roads.element_blocks{ib}{ip}(i_e);
+                        i_e=i_e+1;
+                    end
                 end
             end
+        end
+        
+        function set_coordinates(obj)
+            obj.coordinates=obj.mesh.element_centroids(obj.element_indexes,:);
+        end
+        
+        function set_shapes(obj)
+            obj.shapes(obj.element_indexes,:)=getShapes(obj.afemm.femModel, obj.elementBelonged(k), [obj.cm.state(1,k),...
+                obj.cm.state(3,k)] );
         end
     end
 end
